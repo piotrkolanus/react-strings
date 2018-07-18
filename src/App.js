3b4c6e9710f0e00d6e242bb9fe2e02df76d35a0f
Piotr <piotr.kolanus@gmail.com>
2018-07-18 19:34:18 +0200

    Bug fixed(typo)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
+  state = {
+    userInput: '',
+  };
+
+  inputChangedHandler = event => {
+    this.setState({ userInput: event.target.value });
+  };
+
+  deleteCharHandler = index => {
+    const word = this.state.userInput.split('');
+    word.splice(index, 1);
+    const updatedWord = word.join('');
+    this.setState({ userInput: updatedWord });
+  };
+
   render() {
-    return <div className="App" />;
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return (
+        <Char
+          character={ch}
+          key={index}
+          clicked={() => this.deleteCharHandler(index)}
+        />
+      );
+    });
+    return (
+      <div>
+        <p>Strings</p>
+        <input type="text" onChange={this.inputChangedHandler} />
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
+      </div>
+    );
   }
 }
 
